eee17847f6d4e1e69f6c41b7fcc416d1d5b3498b
petehuan <petehuan@cisco.com>
2015-08-05 14:39:10 +0800

    configure badge detail view except best run

--- SkyRunner/SkyRunner/BadgeDetailViewController.swift
+++ SkyRunner/SkyRunner/BadgeDetailViewController.swift
@@ -9,9 +9,126 @@
 import UIKit
 
 class BadgeDetailViewController: UIViewController {
+    
+    // MARK: - Constants
+    struct NumberDecimalPlaceConstants {
+        static let DecimalPlaceConstants = 3
+    }
+    
+    struct AchievementConstants {
+        static let SilverSpeedFactor = 1.05
+        static let GoldSpeedFactor = 1.1
+    }
+    
+    // MARK: - Variables
     var badgeEarnStatus: BadgeEarnStatus?
     
+    // MARK: - IBOutlets
+    @IBOutlet weak var badgeImageView: UIImageView!
+    @IBOutlet weak var badgeNameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var earnRunLabel: UILabel!
+    @IBOutlet weak var bestRunLabel: UILabel!
+    @IBOutlet weak var silverRunLabel: UILabel!
+    @IBOutlet weak var goldRunLabel: UILabel!
+    @IBOutlet weak var silverImageView: UIImageView!
+    @IBOutlet weak var goldImageView: UIImageView!
+    
+    // MARK: - Override methods
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        
+        configureView()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func showInfo(sender: UIButton) {
+    }
+    
+    // MARK: - Private methods
+    private func configureView() {
+        if badgeEarnStatus == nil {
+            return
+        }
+        
+        configureBadgeImageView()
+        configureLabels()
+        configureAchievementImageViews()
+    }
+    
+    private func configureBadgeImageView() {
+        badgeImageView.image = UIImage(named: badgeEarnStatus!.badge!.imageName!)
+    }
+    
+    private func configureLabels() {
+        configureBadgeNameLabel()
+        configureDistanceLabel()
+        configureEarnRunLabel()
+        configureSilverRunLabel()
+        configureGoldRunLabel()
+    }
+    
+    private func configureAchievementImageViews() {
+        configureSilverAchievementImageView()
+        configureGoldAchievementImageView()
+    }
+    
+    private func configureBadgeNameLabel() {
+        badgeNameLabel.text = badgeEarnStatus?.badge?.name
+    }
+    
+    private func configureDistanceLabel() {
+        distanceLabel.text = "Distance: " + badgeEarnStatus!.badge!.distance!.round(NumberDecimalPlaceConstants.DecimalPlaceConstants).description
+    }
+    
+    private func configureEarnRunLabel() {
+        let timestamp = badgeEarnStatus!.earnRun!.timestamp
+        let dateString = timeFormatter().stringFromDate(timestamp)
+        
+        earnRunLabel.text = "Earned: " + dateString
+    }
+    
+    private func configureSilverRunLabel() {
+        let silverSpeed = (earnRunSpeed() * AchievementConstants.SilverSpeedFactor).round(NumberDecimalPlaceConstants.DecimalPlaceConstants)
+        silverRunLabel.text = "Silver: " + "Run " + silverSpeed.description + " m/s to earn"
+    }
+    
+    private func configureGoldRunLabel() {
+        let goldSpeed = (earnRunSpeed() * AchievementConstants.GoldSpeedFactor).round(NumberDecimalPlaceConstants.DecimalPlaceConstants)
+        goldRunLabel.text = "Gold: " + "Run " + goldSpeed.description + " m/s to earn"
+    }
+    
+    private func configureSilverAchievementImageView() {
+        if badgeEarnStatus?.silverRun != nil {
+            silverImageView.hidden = false
+        }
+        else {
+            silverImageView.hidden = true
+        }
+    }
+    
+    private func configureGoldAchievementImageView() {
+        if badgeEarnStatus?.goldRun != nil {
+            goldImageView.hidden = false
+        }
+        else {
+            goldImageView.hidden = true
+        }
+    }
+    
+    private func timeFormatter() -> NSDateFormatter {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        formatter.timeStyle = .MediumStyle
+        
+        return formatter
+    }
+    
+    private func earnRunSpeed() -> Double {
+        if let earnRun = badgeEarnStatus?.earnRun {
+            return earnRun.distance / earnRun.duration
+        }
+        
+        return 0.0
     }
 }
--- SkyRunner/SkyRunnerTests/BadgeDetailViewControllerTests.swift
+++ SkyRunner/SkyRunnerTests/BadgeDetailViewControllerTests.swift
@@ -8,24 +8,107 @@
 
 import UIKit
 import XCTest
+import CoreData
 
 class BadgeDetailViewControllerTests: XCTestCase {
     
+    var storeCoordinator: NSPersistentStoreCoordinator!
+    var managedObjectContext: NSManagedObjectContext!
+    var managedObjectModel: NSManagedObjectModel!
+    var store: NSPersistentStore!
+    
     var badgeDetailVC: BadgeDetailViewController?
+    var badgeEarnStatus: BadgeEarnStatus?
+    var earnRun: Run?
+    var silverRun: Run?
+    var goldRun: Run?
 
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
+        managedObjectModel = NSManagedObjectModel.mergedModelFromBundles(nil)
+        storeCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        store = storeCoordinator.addPersistentStoreWithType(NSInMemoryStoreType,
+            configuration: nil, URL: nil, options: nil, error: nil)
+        managedObjectContext = NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = storeCoordinator
+        
         var storyBoard = UIStoryboard(name: "Main", bundle: NSBundle(forClass: self.dynamicType))
         badgeDetailVC = storyBoard.instantiateViewControllerWithIdentifier("BadgeDetailViewController") as? BadgeDetailViewController
         badgeDetailVC?.view
+        
+        let badgeJsonString = ["name":"Earth", "imageName":"earth", "distance":"2.01", "information": "badgeInformation1"]
+        var badge = Badge(badgeJsonString: badgeJsonString)
+        badgeEarnStatus = BadgeEarnStatus(badge: badge)
+        
+        earnRun = Run(context: managedObjectContext)
+        earnRun?.timestamp = NSDate(timeIntervalSince1970: 1)
+        earnRun?.duration = 13.0
+        earnRun?.distance = 50.0
+        badgeEarnStatus?.earnRun = earnRun
+        
+        silverRun = Run(context: managedObjectContext)
+        badgeEarnStatus?.silverRun = silverRun
+        
+        goldRun = Run(context: managedObjectContext)
+        badgeEarnStatus?.goldRun = goldRun
+        
+        badgeDetailVC?.badgeEarnStatus = badgeEarnStatus
         badgeDetailVC?.viewWillAppear(false)
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func testImageShouldBeSetCorrectly() {
+        XCTAssertTrue(badgeDetailVC?.badgeImageView.image != nil)
+    }
+    
+    func testBadgeNameShouldBeSetCorrectly() {
+        XCTAssertTrue(badgeDetailVC?.badgeNameLabel.text == "Earth")
+    }
+    
+    func testDistanceLabelShouldBeSetCorrectly() {
+        XCTAssertTrue(badgeDetailVC?.distanceLabel.text == "Distance: 2.01")
+    }
+    
+    func testEarnLabelShouldBeSetCorrectly() {
+        XCTAssertTrue(badgeDetailVC?.earnRunLabel.text == "Earned: Jan 1, 1970, 8:00:01 AM")
+    }
+    
+    func testSilverLabelShouldBeSetCorrectly() {
+        XCTAssertTrue(badgeDetailVC?.silverRunLabel.text == "Silver: Run 4.038 m/s to earn")
+    }
+    
+    func testGoldLabelShouldBeSetCorrectly() {
+        XCTAssertTrue(badgeDetailVC?.goldRunLabel.text == "Gold: Run 4.231 m/s to earn")
+    }
+    
+    func testBestLabelShouldBeSetCorrectly() {
+        XCTFail("fail")
+    }
+    
+    func testSilverImageShouldBeVisibleIfSilverRunIsNotNil() {
+        XCTAssertTrue(badgeDetailVC?.silverImageView.hidden == false)
+    }
+    
+    func testSilverImageShouldBeInvisibleIfSilverRunIsNil() {
+        badgeEarnStatus?.silverRun = nil
+        badgeDetailVC?.viewWillAppear(false)
+        XCTAssertTrue(badgeDetailVC?.silverImageView.hidden == true)
+    }
+    
+    func testGoldImageShouldBeVisibleIfGoldRunIsNotNil() {
+        XCTAssertTrue(badgeDetailVC?.goldImageView.hidden == false)
+    }
+    
+    func testGoldImageShouldBeInvisibleIfGoldRunIsNil() {
+        badgeEarnStatus?.goldRun = nil
+        badgeDetailVC?.viewWillAppear(false)
+        XCTAssertTrue(badgeDetailVC?.goldImageView.hidden == true)
     }
 
 }
