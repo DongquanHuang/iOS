27d8a3d7fbe01e21e67666d7bd15ae0f5611b206
Peter Huang <dongquan_huang@163.com>
2015-08-05 21:21:02 +0800

    add some notes for unit test bug

--- SkyRunner/SkyRunnerTests/BadgeEarnStatusTests.swift
+++ SkyRunner/SkyRunnerTests/BadgeEarnStatusTests.swift
@@ -8,19 +8,62 @@
 
 import UIKit
 import XCTest
+import CoreData
+import SkyRunner
 
 class BadgeEarnStatusTests: XCTestCase {
+    
+    var storeCoordinator: NSPersistentStoreCoordinator!
+    var managedObjectContext: NSManagedObjectContext!
+    var managedObjectModel: NSManagedObjectModel!
+    var store: NSPersistentStore!
     
     var badgeEarnStatus: BadgeEarnStatus?
     var badge: Badge?
     let badgeJsonString = ["name":"badgeName", "imageName":"badgeImageName", "distance":"100.0", "information": "badgeInformation"]
+    
     var badgeEarnStatusMgr = BadgeEarnStatusMgr()
+    
+    var run1: Run?
+    var run2: Run?
+    var run3: Run?
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        managedObjectModel = NSManagedObjectModel.mergedModelFromBundles(nil)
+        storeCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        store = storeCoordinator.addPersistentStoreWithType(NSInMemoryStoreType,
+            configuration: nil, URL: nil, options: nil, error: nil)
+        managedObjectContext = NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = storeCoordinator
+        
+        prepareDatabase()
+        
         badge = Badge(badgeJsonString: badgeJsonString)
         badgeEarnStatus = BadgeEarnStatus(badge: badge!)
+        
+        badgeEarnStatusMgr.managedObjectContext = managedObjectContext
+    }
+    
+    private func prepareDatabase() {
+        run1 = Run(context: managedObjectContext)
+        run1?.timestamp = NSDate(timeIntervalSince1970: 1)
+        run1?.duration = 13.0
+        run1?.distance = 50.0
+        
+        run2 = Run(context: managedObjectContext)
+        run2?.timestamp = NSDate(timeIntervalSince1970: 2)
+        run2?.duration = 113.0
+        run2?.distance = 931.04
+        
+        run3 = Run(context: managedObjectContext)
+        run3?.timestamp = NSDate(timeIntervalSince1970: 3)
+        run3?.duration = 685.0
+        run3?.distance = 1931.04
+        
+        managedObjectContext.save(nil)
     }
     
     override func tearDown() {
@@ -30,6 +73,19 @@ class BadgeEarnStatusTests: XCTestCase {
     
     func testCanInitBadgeEarnStatusWithBadge() {
         XCTAssertTrue(badgeEarnStatus?.badge != nil)
+    }
+    
+    func testCanSetCoreDataContextToMgr() {
+        XCTAssert(badgeEarnStatusMgr.managedObjectContext == managedObjectContext)
+    }
+    
+    // This unit test will cause crash:
+    // caught "NSInvalidArgumentException", "-[Swift._NSContiguousString objCType]: unrecognized selector sent to instance
+    // This is becuase the predicate set for fetch request in product code
+    // Don't know why yet, perhaps Swift bug
+    func testBadgeEarnStatusMgrWillGenerateBadgeEarnStatuses() {
+        //let earnStatuses = badgeEarnStatusMgr.badgeEarnStatuses
+        //XCTAssert(earnStatuses?.count == 33)
     }
     
 }
